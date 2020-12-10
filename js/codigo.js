ff02a2c6230b60dfaaf59e5baafdb9c3783e87a2
210900Diego <75793882+210900Diego@users.noreply.github.com>
2020-12-10 12:44:18 -0500

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,11 @@
+//Desarrollar el codigo de los ejercicios propuestos mediante
+//FUNCIONES
+//Desarrollo01
+function mostrar(){
+    alert("Demostracion de llamar a una funcion");
+}
+
+//Desarrollo02
+
+
+//Desarrollo03
